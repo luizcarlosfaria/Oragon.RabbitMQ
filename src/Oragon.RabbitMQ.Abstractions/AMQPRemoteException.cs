49089cadccfc940d9190f4c1d40558538dd149d9
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2024-05-29 03:12:47 -0300

    Acertando partes deprecated da serialização de exceptions

--- src/Oragon.RabbitMQ.Abstractions/AMQPRemoteException.cs
+++ src/Oragon.RabbitMQ.Abstractions/AMQPRemoteException.cs
@@ -34,19 +34,13 @@ public class AMQPRemoteException : Exception
     /// <param name="message">The error message that explains the reason for the exception.</param>
     /// <param name="remoteStackTrace">The remote stack trace.</param>
     /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
-    public AMQPRemoteException(string message, string remoteStackTrace, Exception inner) : base(message, inner) { this.remoteStackTrace = remoteStackTrace; }
+    public AMQPRemoteException(string message, string remoteStackTrace, Exception inner) : base(message, inner) {
+        this.remoteStackTrace = remoteStackTrace;
+    }
 
     /// <summary>
     /// Gets the string representation of the frames on the call stack at the time the current exception was thrown.
     /// </summary>
     public override string StackTrace => remoteStackTrace;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AMQPRemoteException"/> class with serialized data.
-    /// </summary>
-    /// <param name="info">The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-    /// <param name="context">The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-    protected AMQPRemoteException(
-      System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
