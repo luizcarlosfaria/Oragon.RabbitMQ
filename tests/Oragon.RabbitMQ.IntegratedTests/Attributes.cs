e32fb4a21855602927da3403d8b0496ed22636aa
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2024-05-31 23:48:12 -0300

    Ignroando regras no assembly de testes

---
+++ tests/Oragon.RabbitMQ.IntegratedTests/Attributes.cs
@@ -0,0 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[assembly:System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "Not required here")]
