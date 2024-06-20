c946365900f368b803ef1c72e6e6d42a35cfad6c
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2024-06-20 11:22:58 -0300

    Enhanced Oragon.RabbitMQ with Docker support
    - Added "Standalone", "docker-compose", and "DotNetConsumerApp" projects to the Oragon.RabbitMQ.sln, expanding the solution's capabilities.
    - Updated .dockerignore for better Docker build performance by excluding unnecessary files and directories while allowing specific `.git` files.
    - Introduced a multi-stage Dockerfile for the DotNetConsumerApp, optimizing image size and build time.
    - Upgraded DotNetConsumerApp to target .NET 8.0, enabled nullable reference types, implicit usings, and added package references for RabbitMQ and ASP.NET Core.
    - Added an HTTP client configuration file for API testing in DotNetConsumerApp.
    - Introduced a `PriceChangeEvent` record type and an `UpdateService` class for handling price change events.
    - Updated `Program.cs` with licensing, RabbitMQ setup, and minimal API endpoints.
    - Configured multiple launch profiles in `launchSettings.json` for versatile application testing environments.
    - Updated application settings in `appsettings.json` and `appsettings.Development.json` for logging and allowed hosts.
    - Created a Docker Compose project file (`docker-compose.dcproj`) and updated Docker Compose configurations (`docker-compose.yml` and `docker-compose.override.yml`) for service orchestration.
    - Commented out `PackageType` in Oragon.RabbitMQ.Build.props and set `RequeueOnCrash` to `true` in `AsyncQueueConsumerParameters.cs` for enhanced package handling and fault tolerance.

--- src/Oragon.RabbitMQ/Consumer/AsyncQueueConsumerParameters.cs
+++ src/Oragon.RabbitMQ/Consumer/AsyncQueueConsumerParameters.cs
@@ -71,7 +71,7 @@ public class AsyncQueueConsumerParameters<TService, TRequest, TResponse> : Consu
     /// <summary>
     /// Requeue On Crash
     /// </summary>
-    public bool RequeueOnCrash { get; private set; }
+    public bool RequeueOnCrash { get; private set; } = true;
 
     /// <summary>
     /// KeyOfService on Keyed Services
