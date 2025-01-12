56ef0299f7625c2b6cdbea6f393f2a5483623036
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2025-01-12 05:58:19 -0300

    docker based docs (template only)

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,7 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  output: "standalone",
 }
 
 export default withSearch(
