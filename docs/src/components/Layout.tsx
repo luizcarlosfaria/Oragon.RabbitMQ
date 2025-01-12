903a7f3a6a6b70d100296744217366e5dc75f841
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2025-01-12 07:28:53 -0300

    Docs layout

--- docs/src/components/Layout.tsx
+++ docs/src/components/Layout.tsx
@@ -11,6 +11,8 @@ import { MobileNavigation } from '@/components/MobileNavigation'
 import { Navigation } from '@/components/Navigation'
 import { Search } from '@/components/Search'
 import { ThemeSelector } from '@/components/ThemeSelector'
+import Image from 'next/image'
+import logoImage from '@/images/logo.svg'
 
 function GitHubIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -48,8 +50,12 @@ function Header() {
       </div>
       <div className="relative flex flex-grow basis-0 items-center">
         <Link href="/" aria-label="Home page">
-          <Logomark className="h-9 w-9 lg:hidden" />
-          <Logo className="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-sky-100" />
+          <Image
+            src={logoImage}
+            alt="Logo"
+            width={227}
+            height={36}
+            className="cursor-pointer"></Image>
         </Link>
       </div>
       <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
