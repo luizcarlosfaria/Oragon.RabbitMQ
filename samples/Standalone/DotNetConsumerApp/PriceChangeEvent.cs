d48ec8ca75453ad445501f607e29a6819bb655df
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2025-01-18 18:57:04 -0300

    Removind old standalone example

--- samples/Standalone/DotNetConsumerApp/PriceChangeEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to LuizCarlosFaria, gaGO.io, Mensageria .NET, Cloud Native .NET and ACADEMIA.DEV under one or more agreements.
-// The ACADEMIA.DEV licenses this file to you under the MIT license.
-
-public record PriceChangeEvent
-{
-    public string CurrencyId { get; set; } //BRL
-    public decimal CurrencyValue { get; set; } //5.43
-    public string ReferenceCurrencyId { get; set; }  //USD  
-}
